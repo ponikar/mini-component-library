f8829ad76fa6649961577a9152bb3e93fd59714e
Darshan Ponikar <darshanp@geekyants.com>
2021-11-09 00:06:22 +0530

    exersice done

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,82 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThinkness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThinkness: 2,
+    height: 36,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error("Style prop not found! ");
+  }
+  return (
+    <Wrapper style={{ "--height": styles.height / 16 + "rem" }}>
+      <VisuallyHidden> {label} </VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        style={{
+          "--width": width + "px",
+          "--borderThinkness": styles.borderThinkness + "px",
+          "--fontSize": styles.fontSize / 16 + "rem",
+          "--height": styles.height / 16 + "rem",
+        }}
+        {...delegated}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  margin: auto;
+  height: var(--size);
+`;
+const TextInput = styled.input`
+  height: var(--height);
+  border: none;
+  border-bottom: var(--borderThinkness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  font-size: var(--fontSize);
+  outline-offset: 2px;
+  width: var(--width);
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <RepresentationBit> {displayedValue} </RepresentationBit>
+      <IconWrapper style={{ "--size": "24px" }}>
+        <Icon size={24} strokeWidth={1} id="chevron-down" />
+      </IconWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  --webkit-appearance: none;
+`;
+
+const RepresentationBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  padding: 10px 16px;
+  padding-right: 52px;
+  font-size: 1rem;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  pointer-events: none;
+  right: 10px;
+  top: 0px;
+  bottom: 0px;
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+`;
 
 export default Select;
